2c5c595e6435f30657810f7cb5d607a75efe6457
Jaykingamez <jaykinchan@gmail.com>
2022-01-22 23:52:24 +0800

    Initialized database connection files

---
+++ src/main/java/dao/ApplicationDao.java
@@ -0,0 +1,16 @@
+package dao;
+
+import beans.User;
+
+public class ApplicationDao {
+	
+	public int registerUser(User user) {
+		int rowsAffected = 0;
+		return rowsAffected;
+	}
+	
+	public boolean validateUser(User user) {
+		boolean isValidUser = false;
+		return isValidUser;
+	}
+}
