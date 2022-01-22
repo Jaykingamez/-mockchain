c7f07f6006a84d303e8e4858070d390eba7e31bf
Jaykingamez <jaykinchan@gmail.com>
2022-01-22 23:50:07 +0800

    Added User Bean and LoginServlet and LogoutServlet

---
+++ src/main/java/beans/User.java
@@ -0,0 +1,40 @@
+package beans;
+
+public class User {
+	private String password;
+	private String username;
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public User(String password, String username) {
+		super();
+		this.password = password;
+		this.username = username;
+	}
+}
