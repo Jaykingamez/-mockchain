c5a8c978ba4b6301d4d3f3a52ca734598f96d29d
Jaykingamez <jaykinchan@gmail.com>
2022-01-25 23:33:48 +0800

    Allow user to visit "approve" path to see transactions needing approval

--- src/main/java/servlets/ApproveServlet.java
+++ src/main/java/servlets/ApproveServlet.java
@@ -1,6 +1,7 @@
 package servlets;
 
 import java.io.IOException;
+import java.sql.Connection;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,10 +9,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.ApplicationDao;
+
 @WebServlet("/approve")
 public class ApproveServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ApplicationDao dao = new ApplicationDao();
+		Connection connection = (Connection) getServletContext().getAttribute("dbconnection");
+		request.setAttribute("transactions", dao.getAllTransactions("", connection));
 		request.getRequestDispatcher("/jsp/approve.jsp").forward(request, response);
 	}
 
