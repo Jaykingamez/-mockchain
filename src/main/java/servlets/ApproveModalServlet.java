fa281a9eb2393e437825d1b1589e9a374ec7b27b
Jaykingamez <jaykinchan@gmail.com>
2022-01-26 16:15:53 +0800

    Refactored to use hashmap to store variables

--- src/main/java/servlets/ApproveModalServlet.java
+++ src/main/java/servlets/ApproveModalServlet.java
@@ -2,6 +2,7 @@ package servlets;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +12,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.catalina.connector.Request;
 
+import com.google.gson.Gson;
+
+import beans.User;
 import beans.Wallet;
 import dao.ApplicationDao;
 
@@ -24,10 +28,10 @@ public class ApproveModalServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		HashMap<String, Object> hashMap = new HashMap<String, Object>();
+
 		int walletId = Integer.parseInt(request.getParameter("walletId"));
 		double amount = Double.parseDouble(request.getParameter("amount"));
-		Integer receiverId = Integer.parseInt(request.getParameter("receiverId"));
 		String type = request.getParameter("type");
 
 		ApplicationDao dao = new ApplicationDao();
@@ -35,26 +39,44 @@ public class ApproveModalServlet extends HttpServlet {
 		Wallet walletIdWallet = dao.getWalletWalletId(walletId, connection);
 		Double walletIdAmount = walletIdWallet.getAmount();
 		Double newWalletAmount = walletIdAmount + amount;
-		
-		request.setAttribute("walletId", walletId);
-		request.setAttribute("walletIdAmount", walletIdAmount);
-		request.setAttribute("transactedAmount", amount);
-		request.setAttribute("newWalletAmount", newWalletAmount);
+
+		/*
+		 * request.setAttribute("walletId", walletId);
+		 * request.setAttribute("walletIdAmount", walletIdAmount);
+		 * request.setAttribute("transactedAmount", amount);
+		 * request.setAttribute("newWalletAmount", newWalletAmount);
+		 */
+
+		hashMap.put("walletId", walletId);
+		hashMap.put("walletIdAmount", walletIdAmount);
+		hashMap.put("transactedAmount", amount);
+		hashMap.put("newWalletAmount", newWalletAmount);
 
 		if (type == "transfer") {
+			Integer receiverId = Integer.parseInt(request.getParameter("receiverId"));
 			Wallet receiverIdWallet = dao.getWalletWalletId(receiverId, connection);
 			Double receiverIdAmount = receiverIdWallet.getAmount();
 			// for transfer transaction, amount will be negative,
-			// doing this will sum the amoutn, adding into the receiver's account
+			// doing this will sum the amount, adding into the receiver's account
 			Double newReceiverAmount = receiverIdAmount - amount;
-			
-			request.setAttribute("type", type);
-			request.setAttribute("receiverId", receiverId);
-			request.setAttribute("receiverIdAmount", receiverIdAmount);
-			request.setAttribute("newReceiverAmount", newReceiverAmount);
+
+			/*
+			 * request.setAttribute("type", type); request.setAttribute("receiverId",
+			 * receiverId); request.setAttribute("receiverIdAmount", receiverIdAmount);
+			 * request.setAttribute("newReceiverAmount", newReceiverAmount);
+			 */
+
+			hashMap.put("type", type);
+			hashMap.put("receiverId", receiverId);
+			hashMap.put("receiverIdAmount", receiverIdAmount);
+			hashMap.put("newReceiverAmount", newReceiverAmount);
 		}
-		request.getRequestDispatcher("/jsp/approve.jsp").forward(request, response);
-		
+		// use gson to turn hashmap into json
+		String json = new Gson().toJson(hashMap);
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write(json);
+
 	}
 
 }
