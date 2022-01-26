5ee8f533185d9bde9930a2ea43e0dabb5cb6ffd4
Jaykingamez <jaykinchan@gmail.com>
2022-01-26 23:21:39 +0800

    Use ajax instead of redirecting user

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -2,9 +2,19 @@ function alertFunc(message) {
 	alert(message);
 }
 
-function getInfo(id){
-	var $form = $(id);
-	$.post($form.attr("action"), $form.serialize(), function(response) {
-        // receive the json
-    });
-}
+$(document).ready(function() {
+	$(document).on('submit', '.prevent', function(event) {
+		event.preventDefault();
+		var formId = $(this).attr('id');
+		var $form = $("#"+ formId);
+		$.post($form.attr("action"), $form.serialize(), function(response) {
+			// receive the json
+			$.each(response, function(key, value) {
+				$("#" + key).val(value);
+				console.log('my key' + key);
+				console.log('my value' + value);
+			});
+		});
+		return false;
+	});
+});
