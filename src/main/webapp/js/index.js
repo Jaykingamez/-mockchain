df851d5780c9aeb8a911c47698c891da1c4de1f7
Jaykingamez <jaykinchan@gmail.com>
2022-01-26 16:15:19 +0800

    wip of javascipt ajax to send to approveModalServlet

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,3 +1,10 @@
 function alertFunc(message) {
 	alert(message);
 }
+
+function getInfo(id){
+	var $form = $(id);
+	$.post($form.attr("action"), $form.serialize(), function(response) {
+        // receive the json
+    });
+}
