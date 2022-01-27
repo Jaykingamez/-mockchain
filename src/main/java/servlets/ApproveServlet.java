d44544e6a342ff2ae9204c23b8b810a4fa125ee2
Jaykingamez <jaykinchan@gmail.com>
2022-01-27 23:31:10 +0800

    Update ApproveServlet
    If transaction is negative and transaction is rejected, return deducted
    money
    
    If transaction is positive and transaction is accepted, give money

--- src/main/java/servlets/ApproveServlet.java
+++ src/main/java/servlets/ApproveServlet.java
@@ -8,7 +8,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.rowset.JoinRowSet;
 
+import org.eclipse.jdt.internal.compiler.IDebugRequestor;
+
+import beans.Transaction;
+import beans.Wallet;
 import dao.ApplicationDao;
 
 @WebServlet("/approve")
@@ -16,9 +21,11 @@ public class ApproveServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		int userId = (int) request.getSession().getAttribute("userId");
+		
 		ApplicationDao dao = new ApplicationDao();
 		Connection connection = (Connection) getServletContext().getAttribute("dbconnection");
-		request.setAttribute("transactions", dao.getAllTransactions("", 0, connection));
+		request.setAttribute("transactions", dao.getAllTransactions("", userId, connection));
 		request.getRequestDispatcher("/jsp/approve.jsp").forward(request, response);
 	}
 
@@ -31,8 +38,8 @@ public class ApproveServlet extends HttpServlet {
 			return;
 		}
 		
-		String approve = request.getParameter("approve");
-		String reject = request.getParameter("reject");
+		String bool = request.getParameter("bool");
+		int walletId = Integer.parseInt(request.getParameter("hiddenWalletId"));
 		int transactionId = Integer.parseInt(request.getParameter("transactionId"));
 		int userId = (int) request.getSession().getAttribute("userId");
 		
@@ -44,13 +51,29 @@ public class ApproveServlet extends HttpServlet {
 		// get the number of users at 50%
 		int halfNumberOfUsers = Math.floorDiv(numberOfUsers, 2);
 		
-		if(approve != null) {
+		// should also work on a transfer use case
+		
+		if(bool == "approve") {
 			int approveBool = 1;
 			dao.addApprove(transactionId, userId, approveBool, connection);
 			int numberOfApprovals = dao.getApprove(transactionId,approveBool, connection);
 			if(numberOfApprovals >= halfNumberOfUsers) {
 				//approve transaction
 				dao.updateTransactionApproval(transactionId, approveBool, connection);
+				
+				Wallet wallet = dao.getWalletWalletId(walletId, connection);
+				Transaction transaction = dao.getTransaction(walletId, connection);
+				
+				double walletAmount = wallet.getAmount();
+				double transactionAmount = transaction.getAmount();
+				double newAmount = walletAmount + transactionAmount;
+				
+				// if transaction is negative, amount was already deducted
+				if(transactionAmount < 0) {
+					return;
+				}
+				
+				dao.updateWalletAmount(walletId, newAmount, connection);
 			}
 		} else {
 			int rejectBool = 0;
@@ -59,9 +82,20 @@ public class ApproveServlet extends HttpServlet {
 			if(numberOfRejections >= halfNumberOfUsers) {
 				//reject transaction
 				dao.updateTransactionApproval(transactionId, rejectBool, connection);
+				
+				Wallet wallet = dao.getWalletWalletId(walletId, connection);
+				Transaction transaction = dao.getTransaction(walletId, connection);
+				
+				double walletAmount = wallet.getAmount();
+				double transactionAmount = transaction.getAmount();
+				
+				// if transaction is negative, return deducted amount
+				if(transactionAmount < 0) {
+					double newAmount = walletAmount - transactionAmount;
+					dao.updateWalletAmount(walletId, newAmount, connection);
+				}
 			}
 		}
-		
-		doGet(request, response);
+		response.sendRedirect("approve");
 	}
 }
