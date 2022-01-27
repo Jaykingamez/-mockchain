d93c5836bf20210df93cd42b360b2080600b7151
Jaykingamez <jaykinchan@gmail.com>
2022-01-27 23:29:30 +0800

    Add the inclusion of walletId for dao.ApplicationDao methods

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -6,26 +6,26 @@ $(document).ready(function() {
 	$(document).on('submit', '.prevent', function(event) {
 		event.preventDefault();
 		var formId = $(this).attr('id');
-		var $form = $("#"+ formId);
+		var $form = $("#" + formId);
 		console.log($form.attr("action"));
-		/*
 		$.post($form.attr("action"), $form.serialize(), function(response) {
 			console.log(response);
 			// receive the json
-			if(response.type === undefined){
+			if (response.type === undefined) {
 				$("#receiver").hide();
 			}
 			$.each(response, function(key, value) {
-				if (key.includes("Amount")){
+				if (key.includes("Amount")) {
 					$("#" + key).html(value + ".00");
-				} else if(key === "transactionId") 
+				} else if (key === "transactionId")
 					$("#" + key).val(value);
-				else{
+				else if(key === "walletId")
+					$("#hiddenWalletId").val(value);
+				else {
 					$("#" + key).html(value);
 				}
 			});
 		});
 		return false;
-		*/
 	});
 });
