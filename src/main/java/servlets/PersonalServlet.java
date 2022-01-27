a57c3100567f1de9a2eeea2e0e00d72fbb538b44
Jaykingamez <jaykinchan@gmail.com>
2022-01-27 23:30:04 +0800

    if transaction is negative, preemptively subtract from wallet

--- src/main/java/servlets/PersonalServlet.java
+++ src/main/java/servlets/PersonalServlet.java
@@ -11,8 +11,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.jasper.tagplugins.jstl.core.If;
+
 import beans.Approve;
 import beans.Transaction;
+import beans.Wallet;
 import dao.ApplicationDao;
 
 /**
@@ -42,35 +45,42 @@ public class PersonalServlet extends HttpServlet {
 			String operator = request.getParameter("operator");
 			double amount = Double.parseDouble(request.getParameter("amount"));
 			int walletId = (int) request.getSession().getAttribute("walletId");
-			
-			// deducting the amount makes it negative, else leave it as positive
-			if (operator== "-") {
-				amount = -amount;
-			}
-			
+
 			ApplicationDao dao = new ApplicationDao();
 			Connection connection = (Connection) getServletContext().getAttribute("dbconnection");
-			
+
 			// Get last transaction Id so that it can be chained to the new transaction
 			// return 0 by default if no transactions exist in the table
 			Integer lastTransactionId = dao.getLastTransactionId(walletId, connection);
-			
+
 			int transactionId = lastTransactionId + 1;
 			if (lastTransactionId == -1) {
 				lastTransactionId = null;
-			} 
+			}
 			
-			Transaction transaction = new Transaction(transactionId, lastTransactionId, new Timestamp(System.currentTimeMillis()),
-					walletId, null, amount, "personal", null);
+			String infoMessage = "Transaction performed successfully!";
+			// deducting the amount makes it negative, else leave it as positive
+			if (operator == "-") {
+				Wallet wallet =  dao.getWalletWalletId(walletId, connection);
+				if(wallet.getAmount() < amount){
+					infoMessage = "You don't have that much funds in your wallet!";
+				} else {
+					// stake the money so it cant be used for other transactions
+					dao.updateWalletAmount(walletId, wallet.getAmount() - amount , connection);
+				}
+				amount = -amount;
+			}
+
+			Transaction transaction = new Transaction(transactionId, lastTransactionId,
+					new Timestamp(System.currentTimeMillis()), walletId, null, amount, "personal", null);
 			int transactionAffected = dao.addTransaction(transaction, connection);
 
 			transaction = dao.getTransaction(walletId, connection);
 
-			String infoMessage = null;
 			if (transactionAffected == -1 || transaction == null) {
 				infoMessage = "Sorry, we failed to perform that transaction!";
-				request.setAttribute("infoMessage", infoMessage);
-			} 
+			}
+			request.setAttribute("infoMessage", infoMessage);
 		} catch (NumberFormatException exception) {
 			exception.printStackTrace();
 		}
