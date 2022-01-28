f241f450f1adcc0d171cbf9330534f9f7074f400
Jaykingamez <jaykinchan@gmail.com>
2022-01-28 12:23:26 +0800

    retrieve latest wallet amount of current user

--- src/main/java/servlets/WalletServlet.java
+++ src/main/java/servlets/WalletServlet.java
@@ -1,11 +1,16 @@
 package servlets;
 
 import java.io.IOException;
+import java.sql.Connection;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import beans.Wallet;
+import dao.ApplicationDao;
 
 /**
  * Servlet implementation class WalletServlet
@@ -19,6 +24,15 @@ public class WalletServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int walletId = (int) request.getSession().getAttribute("walletId");
+		
+		ApplicationDao dao = new ApplicationDao();
+		Connection connection = (Connection) getServletContext().getAttribute("dbconnection");
+		
+		Wallet wallet = dao.getWalletWalletId(walletId, connection);
+		Double amount = wallet.getAmount();
+		
+		request.getSession().setAttribute("amount", amount);
 		request.getRequestDispatcher("/jsp/wallet.jsp").forward(request, response);
 	}
 
