a946e5cdea8ddf43a5a0c8db9cea734150e0e15c
Jaykingamez <jaykinchan@gmail.com>
2022-02-14 14:33:53 +0800

    Added UserDaoTest

--- src/test/java/mockchain/UserDaoTest.java
+++ src/test/java/mockchain/UserDaoTest.java
@@ -5,10 +5,16 @@ package mockchain;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.intThat;
 import static org.mockito.Mockito.any;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import javax.servlet.http.HttpServletRequest;
@@ -35,10 +41,9 @@ import servlets.RegisterServlet;
  */
 class UserDaoTest {
 
-	
 	private String testUsername = "testUsername";
 	private String testPassword = "testPassword";
-	
+
 	private static Connection connection;
 	private ApplicationDao dao;
 	private User testUser;
@@ -49,16 +54,17 @@ class UserDaoTest {
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		connection = TestDBConnection.getConnectionToDatabase();
-		
+
 		connection.createStatement().execute("CREATE SCHEMA mockchain ;");
-		connection.createStatement().execute("CREATE TABLE user ( \r\n"
-				+ "   userId INT UNSIGNED NOT NULL AUTO_INCREMENT, \r\n"
-				+ "   username VARCHAR(50) NOT NULL, \r\n"
-				+ "   password VARCHAR(50) NOT NULL, \r\n"
-				+ "	  PRIMARY KEY (`userId`),"
-				+ "   UNIQUE INDEX `username_UNIQUE` (`username` ASC)"
-				+ ");");
-		
+		connection.createStatement()
+				.execute("CREATE TABLE `user` (\r\n"
+						+ "  `userId` INT UNSIGNED NOT NULL AUTO_INCREMENT,\r\n"
+						+ "  `username` VARCHAR(50) NOT NULL,\r\n"
+						+ "  `password` VARCHAR(50) NOT NULL,\r\n"
+						+ "  PRIMARY KEY (`userId`),\r\n"
+						+ "  UNIQUE INDEX `username_UNIQUE` (`username` ASC));");
+
+
 	}
 
 	/**
@@ -68,7 +74,7 @@ class UserDaoTest {
 	static void tearDownAfterClass() throws Exception {
 		connection.createStatement().execute("DROP ALL OBJECTS DELETE FILES");
 	}
-	
+
 	/**
 	 * @throws java.lang.Exception
 	 */
@@ -77,17 +83,33 @@ class UserDaoTest {
 		dao = new ApplicationDao();
 		testUser = new User(testUsername, testPassword);
 	}
-	
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+
+	@AfterEach
+	void tearDown() throws Exception {
+		// remove the contents of the table
+		connection.createStatement().execute("truncate table user RESTART IDENTITY;");
+		// reset auto_increment ids to 1
+		connection.createStatement().execute("ALTER table user alter column userId RESTART WITH 1;");
+	}
 
 	/**
 	 * Test method for
 	 * {@link dao.ApplicationDao#registerUser(beans.User, java.sql.Connection)}.
+	 * 
+	 * @throws SQLException
 	 */
 	@Test
-	void testRegisterUser() {
+	void testRegisterUser() throws SQLException {
 		int result = dao.registerUser(testUser, connection);
 		assertEquals(result, 1);
-		
+
+		// validate user
+		result = dao.validateUser(testUser, connection);
+		assertEquals(result, 1);
 	}
 
 	/**
@@ -96,12 +118,34 @@ class UserDaoTest {
 	 */
 	@Test
 	void testValidateUser() {
-		int result = dao.validateUser(testUser, connection);
+		// add user
+		int result = dao.registerUser(testUser, connection);
 		assertEquals(result, 1);
-		
+
+		// validate user
+		result = dao.validateUser(testUser, connection);
+		assertEquals(result, 1);
+
+		// test using a user that does not exist
 		User fakeUser = new User(testUsername, testUsername);
 		result = dao.validateUser(fakeUser, connection);
 		assertEquals(result, -1);
+	}
+
+	/**
+	 * Test method for
+	 * {@link dao.ApplicationDao#getTotalNumberOfUsers(java.sql.Connection)}.
+	 */
+	@Test
+	void testGetTotalNumberOfUsers() {
+		int number = dao.getTotalNumberOfUsers(connection);
+		assertEquals(0, number);
+
+		int result = dao.registerUser(testUser, connection);
+		assertEquals(1, result);
+
+		number = dao.getTotalNumberOfUsers(connection);
+		assertEquals(1, number);
 	}
 
 }
