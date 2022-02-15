53542a547404cbf314c1726322f1a93d2e526759
Jaykingamez <jaykinchan@gmail.com>
2022-02-15 17:01:28 +0800

    Move Bean Test to new package

---
+++ src/test/java/beans/UserTest.java
@@ -0,0 +1,57 @@
+package beans;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import beans.User;
+
+class UserTest {
+	
+	private User userTest;
+	private String username;
+	private String password;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		username = "username";
+		password = "password";
+		userTest = new User(username, password);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testUser() {
+		assertEquals(userTest.getUsername(), username);
+		assertEquals(userTest.getPassword(), password);
+	}
+
+	@Test
+	void testGetPassword() {
+		assertEquals(userTest.getPassword(), password);
+		
+	}
+
+	@Test
+	void testSetPassword() {
+		userTest.setPassword(username);
+		assertEquals(userTest.getPassword(), username);
+	}
+
+	@Test
+	void testGetUsername() {
+		assertEquals(userTest.getUsername(), username);
+	}
+
+	@Test
+	void testSetUsername() {
+		userTest.setUsername(password);
+		assertEquals(userTest.getUsername(), password);
+	}
+
+}
---
+++ src/test/java/beans/WalletTest.java
@@ -0,0 +1,105 @@
+/**
+ * 
+ */
+package beans;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import beans.Wallet;
+
+/**
+ * @author jayki
+ *
+ */
+class WalletTest {
+	private int walletId;
+	private int userId;
+	private double amount;
+
+	private Wallet testWallet;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+		walletId = 1;
+		userId = 1;
+		amount = 0;
+
+		testWallet = new Wallet(walletId, userId, amount);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link beans.Wallet#Wallet(int, int, double)}.
+	 */
+	@Test
+	void testWallet() {
+		assertEquals(testWallet.getWalletId(), walletId);
+		assertEquals(testWallet.getUserId(), userId);
+		assertEquals(testWallet.getAmount(), amount);
+	}
+
+	/**
+	 * Test method for {@link beans.Wallet#getWalletId()}.
+	 */
+	@Test
+	void testGetWalletId() {
+		assertEquals(testWallet.getWalletId(), walletId);
+	}
+
+	/**
+	 * Test method for {@link beans.Wallet#setWalletId(int)}.
+	 */
+	@Test
+	void testSetWalletId() {
+		testWallet.setWalletId(walletId + 1);
+		assertEquals(testWallet.getWalletId(), walletId + 1);
+	}
+
+	/**
+	 * Test method for {@link beans.Wallet#getUserId()}.
+	 */
+	@Test
+	void testGetUserId() {
+		assertEquals(testWallet.getUserId(), userId);
+	}
+
+	/**
+	 * Test method for {@link beans.Wallet#setUserId(int)}.
+	 */
+	@Test
+	void testSetUserId() {
+		testWallet.setUserId(userId + 1);
+		assertEquals(testWallet.getUserId(), userId + 1);
+	}
+
+	/**
+	 * Test method for {@link beans.Wallet#getAmount()}.
+	 */
+	@Test
+	void testGetAmount() {
+		assertEquals(testWallet.getAmount(), amount);
+	}
+
+	/**
+	 * Test method for {@link beans.Wallet#setAmount(double)}.
+	 */
+	@Test
+	void testSetAmount() {
+		testWallet.setAmount(amount + 1);
+		assertEquals(testWallet.getAmount(), amount + 1);
+	}
+
+}
