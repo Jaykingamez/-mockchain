87111a1c12a4843e3fde861896884c00dfcbc8a9
Jaykingamez <jaykinchan@gmail.com>
2022-02-19 11:30:22 +0800

    add initializedatabase and destroy database methods

--- src/main/java/dao/DBConnection.java
+++ src/main/java/dao/DBConnection.java
@@ -33,4 +33,48 @@ public class DBConnection {
 		}
 		return connection;
 	}
+	
+	public static void initializeDatabase(Connection connection) {
+		try {
+			//connection.createStatement().execute("CREATE SCHEMA mockchain ;");
+			connection.createStatement()
+					.execute("CREATE TABLE `mockchain`.`user` (\r\n" + "  `userId` INT UNSIGNED NOT NULL AUTO_INCREMENT,\r\n"
+							+ "  `username` VARCHAR(50) NOT NULL,\r\n" + "  `password` VARCHAR(50) NOT NULL,\r\n"
+							+ "  PRIMARY KEY (`userId`),\r\n" + "  UNIQUE INDEX `username_UNIQUE` (`username` ASC));");
+			connection.createStatement()
+					.execute("CREATE TABLE `mockchain`.`wallet` (\r\n" + "  `walletId` INT NOT NULL AUTO_INCREMENT,\r\n"
+							+ "  `userId` INT UNSIGNED NOT NULL,\r\n" + "  `amount` DECIMAL(65,2) NOT NULL,\r\n"
+							+ "  UNIQUE INDEX `userId_UNIQUE` (`userId` ASC),\r\n" + "  PRIMARY KEY (`walletId`),\r\n"
+							+ "  UNIQUE INDEX `walletId_UNIQUE` (`walletId` ASC));\r\n");
+			connection.createStatement()
+					.execute("CREATE TABLE `mockchain`.`transaction` (\r\n"
+							+ "  `transactionId` INT UNSIGNED NOT NULL AUTO_INCREMENT,\r\n"
+							+ "  `previousTransactionId` INT NULL,\r\n" + "  `timestamp` TIMESTAMP NOT NULL,\r\n"
+							+ "  `walletId` INT UNSIGNED NOT NULL,\r\n" + "  `receiverId` INT UNSIGNED NULL,\r\n"
+							+ "  `amount` DECIMAL(65,2) NOT NULL,\r\n" + "  `type` VARCHAR(45) NOT NULL,\r\n"
+							+ "  `approve` TINYINT NULL,\r\n" + "  PRIMARY KEY (`transactionId`),\r\n"
+							+ "  UNIQUE INDEX `transactionId_UNIQUE` (`transactionId` ASC),\r\n"
+							+ "  UNIQUE INDEX `previousTransactionId_UNIQUE` (`previousTransactionId` ASC)" + ");");
+			connection.createStatement()
+					.execute("CREATE TABLE `mockchain`.`approve` (\r\n" + "  `approveId` INT UNSIGNED NOT NULL AUTO_INCREMENT,\r\n"
+							+ "  `transactionId` INT UNSIGNED NOT NULL,\r\n" + "  `userId` INT UNSIGNED NOT NULL,\r\n"
+							+ "  `approve` TINYINT(1) NULL,\r\n" + "  PRIMARY KEY (`approveId`),\r\n"
+							+ "  UNIQUE INDEX `approveId_UNIQUE` (`approveId` ASC));");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void destroyDatabase(Connection connection) {
+		try {
+			String[] tableList = {"user", "wallet", "transaction", "approve"};
+			for (String table: tableList) {
+				connection.createStatement().execute("USE mockchain");
+				connection.createStatement().execute("truncate table " + table);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
